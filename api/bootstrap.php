a8c37f1ef31e19168717e24ee9e52c5f2fc82a65
eduardovk <eduardo_vk@hotmail.com>
2020-06-29 16:04:59 -0300

    API movida para pasta api, msgs de erro corrigidas

---
+++ api/bootstrap.php
@@ -0,0 +1,94 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Psr7Middlewares\Middleware\TrailingSlash;
+
+$configs = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$container = new \Slim\Container($configs);
+
+$container['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) use ($c) {
+        $statusCode = $exception->getCode() ? $exception->getCode() : 500;
+        return $c['response']->withStatus($statusCode)
+        ->withHeader('Content-Type', 'Application/json')
+        ->withJson(["message" => $exception->getMessage()], $statusCode);
+    };
+};
+
+$container['notAllowedHandler'] = function ($c) {
+    return function ($request, $response, $methods) use ($c) {
+        return $c['response']
+            ->withStatus(405)
+            ->withHeader('Allow', implode(', ', $methods))
+            ->withHeader('Content-Type', 'Application/json')
+            ->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+            ->withJson(["message" => "Method not Allowed; Method must be one of: " . implode(', ', $methods)], 405);
+    };
+};
+
+$container['notFoundHandler'] = function ($container) {
+    return function ($request, $response) use ($container) {
+        return $container['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'Application/json')
+            ->withJson(['message' => 'Page not found']);
+    };
+};
+
+$container['logger'] = function($container) {
+    $logger = new Monolog\Logger('logs');
+    $logfile = __DIR__ . '/log/logs.log';
+    $stream = new Monolog\Handler\StreamHandler($logfile, Monolog\Logger::DEBUG);
+    $fingersCrossed = new Monolog\Handler\FingersCrossedHandler(
+        $stream, Monolog\Logger::INFO);
+    $logger->pushHandler($fingersCrossed);
+
+    return $logger;
+};
+
+$isDevMode = true;
+
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Models/Entity"), $isDevMode);
+
+$conn = array('driver' => 'pdo_mysql','host'=> 'localhost','dbname' => 'trab2fea','user' => 'root','password' => '');
+
+$entityManager = EntityManager::create($conn, $config);
+
+$container['em'] = $entityManager;
+
+$container['secretkey'] = "chavesecreta";
+
+$app = new \Slim\App($container);
+
+$app->add(new TrailingSlash(false));
+
+/*
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    "users" => [
+        "eduardo" => "abc123"
+    ],
+    "path" => ["/auth"],
+]));*/
+$app->add(new \Tuupola\Middleware\HttpBasicAuthentication([
+    "users" => [
+        "eduardo" => "abc123"
+    ],
+    "path" => ["/v1/auth"],
+]));
+
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    "regexp" => "/(.*)/", //Regex para encontrar o Token nos Headers - Livre
+    "header" => "X-Token", //O Header que vai conter o token
+    "path" => "/", //Vamos cobrir toda a API a partir do /
+    "ignore" => ["/auth", "/v1/auth"], //Vamos adicionar a exceção de cobertura a rota /auth
+    "realm" => "Protected",
+    "secret" => $container['secretkey'] //Nosso secretkey criado
+]));
---
+++ api/src/Models/Entity/Inscricao.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models\Entity;
+
+/**
+* @Entity @Table(name="inscricoes")
+**/
+class Inscricao {
+
+    /**
+    * @var int
+    * @Id @Column(type="integer")
+    * @GeneratedValue
+    */
+    public $id;
+
+    /**
+    * @var int
+    * @Column(type="integer")
+    */
+    public $id_usuario;
+
+    /**
+    * @var int
+    * @Column(type="integer")
+    */
+    public $id_ingresso;
+
+    /**
+     * @var string
+     * @Column(type="string")
+     */
+    public $nome;
+
+    /**
+    * @var bool
+    * @Column(type="boolean")
+    */
+    public $pago;
+
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getIdUsuario(){
+        return $this->id_usuario;
+    }
+
+    public function setIdUsuario($id_usuario){
+        if (!$id_usuario) {
+            throw new \InvalidArgumentException("É necessário informar a ID do usuário da inscrição!", 400);
+        }
+        $this->id_usuario = $id_usuario;
+        return $this;
+    }
+
+    public function getIdIngresso(){
+        return $this->id_ingresso;
+    }
+
+    public function setIdIngresso($id_ingresso){
+        if (!$id_ingresso) {
+            throw new \InvalidArgumentException("É necessário informar a ID do ingresso da inscrição!", 400);
+        }
+        $this->id_ingresso = $id_ingresso;
+        return $this;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        if (!$nome && !is_string($nome)) {
+            throw new \InvalidArgumentException("É necessário informar o nome na inscrição!", 400);
+        }
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getPago(){
+        return $this->pago;
+    }
+
+    public function setPago($pago){
+        $this->pago = $pago;
+        return $this;
+    }
+
+
+
+}
